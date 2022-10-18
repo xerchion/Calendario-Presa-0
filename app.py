fef99e4a8abedc5357597295dab27a436237435c
Xerchio <xerchion@gmail.com>
2022-10-18 14:05:53 +0200

    mas cosas

--- app.py
+++ app.py
@@ -2,44 +2,17 @@
 from flask import Flask, render_template, request, redirect, session, url_for,flash
 
 from datetime import date
-
-
-import calendarioReal
-import Usuarios
-import gestionBD
 import time
 
 import formularios
-
-
-usuario=Usuarios.Usuario()
-
-
 turno=""
-
-
-
-
-
 year=date.today().year
 
-colores={"A":"bg-success", "B":"bg-primary", "C":"bg-danger" ,"D":"bg-warning" ,"E":"bg-warning bg-opacity-50" }
-coloresdias={"N": "bg-secondary","T":"bg-warning" ,"M":"bg-info" } #Mañana tarde noche
-nombreUsuarioActivo="Invitado"
-
-
-# Con esta linea creamos la instancia de la clase Flask a nuestro objeto llamado app que será
-#la aplicación en si.
 app=Flask(__name__)
 
 #Creamos el index de la app, utilizando templates, en html desde la carpteta templates
 @app.route("/", methods=["POST","GET"])
 def index():
-
-
-
-
-    
     nombreUsuario="Invitado"
 
     datosCalendario=formularios.Datos(request.form)
@@ -47,7 +20,7 @@ def index():
     if request.method=='POST': #el llamar a validate, parece estar obsoleto, ya lo hace en el formulario
         print("Soy index y llega por post")
     else:
-        print("llega por otra cosa",year,turno,nombreUsuarioActivo)
+        print("llega por otra cosa",year,turno)
         #validamos
         
    
