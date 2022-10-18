393ee460e73ddffe45e5edcd4a82710c1b8ec9f5
Xerchio <xerchion@gmail.com>
2022-10-18 16:43:03 +0200

    menos aun

--- app.py
+++ app.py
@@ -6,15 +6,8 @@ app=Flask(__name__)
 #Creamos el index de la app, utilizando templates, en html desde la carpteta templates
 @app.route("/", methods=["POST"])
 def index():
-    nombreUsuario="Invitado"
 
-    
-
-        #validamos
-        
-   
-
-    return render_template("index.html",,year=2022,turno="C",colores="green",nombre="invitado")
+    return "hola"
 # fin iniciar sesion
 if __name__=="__main__":
     app.run(debug=True)
